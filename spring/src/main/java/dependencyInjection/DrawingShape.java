2a62837885c45440f8e5b11461298deb563b3113
piyali <piyali.gb@gmail.com>
2018-10-16 06:58:58 +0530

    Initial checkin

--- spring/src/main/java/dependencyInjection/DrawingShape.java
+++ spring/src/main/java/dependencyInjection/DrawingShape.java
@@ -1,11 +1,8 @@
 package dependencyInjection;
 
 public class DrawingShape {
-    private Shape shape = new Shape() {
-        public void draw() {
-            System.out.println("Inside DrawingShape");
-        }
-    };
+    private Shape shape;
+
     public void setShape(Shape shape){
         this.shape = shape;
     }
